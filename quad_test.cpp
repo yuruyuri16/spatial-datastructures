b75a9d23ec13b26b17b759e93f79a10dd227b227
Rommel Quintanilla <rquintanilla@utec.edu.pe>
2021-06-24 06:18:57 -0500

    Added boost rtree testing

--- quad_test.cpp
+++ quad_test.cpp
@@ -5,6 +5,12 @@
 #include "Rectangle.h"
 #include "QuadTree.h"
 
+#include <boost/geometry.hpp>
+#include <boost/geometry/geometries/point.hpp>
+#include <boost/geometry/geometries/box.hpp>
+#include <boost/geometry/index/rtree.hpp>
+#include <boost/shared_ptr.hpp>
+
 using namespace utec::spatial;
 
 class QuadTreeTest : public ::testing::Test {
@@ -62,11 +68,29 @@ T genRandomNumber(T startRange, T endRange)
     return startRange + (T)rand()/((T)RAND_MAX/(T)(endRange-startRange));
 }
 
+namespace bg = boost::geometry;
+namespace bgi = boost::geometry::index;
+
+namespace boost{ namespace geometry{ namespace index{
+template <typename Box>
+struct indexable<boost::shared_ptr<Box>>{
+  typedef boost::shared_ptr<Box> V;
+  typedef Box const& result_type;
+  result_type operator()(V const& v) const {return *v;}
+};
+}}} //boost::geometry::index
+
+typedef bg::model::d2::point_xy<double> point;
+typedef boost::shared_ptr<point> value;
+typedef bg::model::box<point> box;
+
 class QuadTreeParamTest : public ::testing::TestWithParam<std::size_t> {
   protected:
     using data_t = int;
     using point_t = Point<data_t, 2>;
     QuadTree<QuadNode<point_t>, Rectangle<point_t>, point_t> tree;
+
+    bgi::rtree<value, bgi::linear<16, 4>> rtree;
 };
 
 TEST_P(QuadTreeParamTest, randomRangeTest) {
@@ -79,13 +103,19 @@ TEST_P(QuadTreeParamTest, randomRangeTest) {
 
   for(auto& p : points){
     tree.insert(p);
+
+    boost::shared_ptr<point> new_point(new point(p.get(0), p.get(1)));
+    rtree.insert(new_point);
   }
 
   Rectangle<point_t> region({{200, 200}}, {{600, 600}});
-
   auto result = tree.range(region);
 
-  EXPECT_NE(result.size(), 0);
+  box query(point(200, 200), point(600, 600));
+  std::vector<value> reference_result;
+  rtree.query(bgi::intersects(query), std::back_inserter(reference_result));
+
+  EXPECT_EQ(result.size(), reference_result.size());
 }
 
 INSTANTIATE_TEST_CASE_P(
